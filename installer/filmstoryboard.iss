7d635a8fb410f321f0ef7a1ca5ba8201d15a1890
luojiang419 <luojiang419@users.noreply.github.com>
2026-08-24 12:22:43 +0800

    chore: bump release version to 1.0.0.339

--- installer/filmstoryboard.iss
+++ installer/filmstoryboard.iss
@@ -1,5 +1,5 @@
 #define MyAppName "filmstoryboard"
-#define MyAppVersion "1.0.0.338"
+#define MyAppVersion "1.0.0.339"
 #define MyAppPublisher "Jiang"
 #define MyAppExeName "filmstoryboard.exe"
 
--- lib/features/updater/domain/app_update_config.dart
+++ lib/features/updater/domain/app_update_config.dart
@@ -3,8 +3,8 @@ class AppUpdateConfig {
 
   static const appName = 'filmstoryboard';
   static const userAgent = 'filmstoryboard';
-  static const currentVersion = '1.0.0.338';
-  static const currentVersionTag = 'v1.0.0.338';
+  static const currentVersion = '1.0.0.339';
+  static const currentVersionTag = 'v1.0.0.339';
   static const windowTitle = '$appName $currentVersionTag';
   static const installerBaseName = 'filmstoryboard-Setup';
   static const defaultReleaseRepositoryUrl =
